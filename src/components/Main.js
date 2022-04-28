7c4e78484713afc572ff2b6dd29eb4a8cd5fc7f0
Dmitry Ilin <dm-ilin@mail.ru>
2022-04-28 23:11:15 +0400

    Добавить императивные обработчики открытия модальных окон

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,27 @@
 function Main() {
+  function handleEditAvatarClick () {
+    const popupChangeAvatar = document.querySelector('.popup_sec_avatar');
+    popupChangeAvatar.classList.add('popup_opened');
+  };
+  function handleEditProfileClick () {
+    const popupProfile = document.querySelector('.popup_sec_profile');
+    popupProfile.classList.add('popup_opened');
+  };
+  function handleAddPlaceClick () {
+    const popupNewCard = document.querySelector('.popup_sec_new');
+    popupNewCard.classList.add('popup_opened');
+  };
+
   return (
     <main>
       <section className="profile">
-        <div className="profile__avatar"></div>
+        <div className="profile__avatar" onClick={handleEditAvatarClick}></div>
         <div className="profile__info">
           <h1 className="profile__info-title">Кусто</h1>
-          <button className="profile__info-edit-button" type="button"></button>
+          <button className="profile__info-edit-button" onClick={handleEditProfileClick} type="button"></button>
           <p className="profile__info-status">Иследователь</p>
         </div>
-        <button className="profile__button" type="button"></button>
+        <button className="profile__button" onClick={handleAddPlaceClick} type="button"></button>
       </section>
 
       <section className="elements"></section>
